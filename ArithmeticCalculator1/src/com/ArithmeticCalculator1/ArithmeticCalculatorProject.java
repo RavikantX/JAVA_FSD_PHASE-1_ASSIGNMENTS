e154b782a04121eacb415dace80f44e28b226a50
RavikantX <ravikant97087@gmail.com>
2022-02-01 14:19:41 +0530

    Arithmetic Calculator1_Project Phase 1

---
+++ ArithmeticCalculator1/src/com/ArithmeticCalculator1/ArithmeticCalculatorProject.java
@@ -0,0 +1,64 @@
+package com.ArithmeticCalculator1;
+
+import java.util.Scanner;
+
+public class ArithmeticCalculatorProject {
+
+	public static void main(String[] args) {
+	
+		 char operator;
+		    Double number1, number2, result;
+
+		    // create an object of Scanner class
+		    Scanner input = new Scanner(System.in);
+
+		    // ask users to enter operator so that user can perform operation of its own choice
+		    System.out.println("Choose an operator: +, -, *, or /");
+		    operator = input.next().charAt(0);
+
+		    // ask users to enter numbers
+		    System.out.println("Enter first number");
+		    number1 = input.nextDouble();
+
+		    System.out.println("Enter second number");
+		    number2 = input.nextDouble();
+            
+		    // Here we will use switch
+		    switch (operator) {
+
+		      // performs addition between numbers
+		      case '+':
+		        result = number1 + number2;
+		        System.out.println(number1 + " + " + number2 + " = " + result);
+		        break;
+
+		      // performs subtraction between numbers
+		      case '-':
+		        result = number1 - number2;
+		        System.out.println(number1 + " - " + number2 + " = " + result);
+		        break;
+
+		      // performs multiplication between numbers
+		      case '*':
+		        result = number1 * number2;
+		        System.out.println(number1 + " * " + number2 + " = " + result);
+		        break;
+
+		      // performs division between numbers
+		      case '/':
+		        result = number1 / number2;
+		        System.out.println(number1 + " / " + number2 + " = " + result);
+		        break;
+		    // Apart from above operation we will define a default case also 
+		      default:
+		        System.out.println("Invalid operator!");
+		        break;
+		    }
+
+		    input.close();
+		  }
+		
+
+	}
+
+
